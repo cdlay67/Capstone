2bfbe15b8844ce1f002e292eeab0585fe0457835
cdlay67 <dclaydowning@gmail.com>
2018-12-04 03:20:25 -0600

    staging second commit

--- index.js
+++ index.js
@@ -7,152 +7,19 @@ $(document).ready(function () {
         });
         $("#charactersname").text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
         $(this).css("background-color", "#00cc99");
+
+        var characterSelect = $("#characters").attr('class');
+        
+        if (form === null) {
+            alert('Please select a character');
+        }
+        else {
+            document.getElementById("characters").innerHTML = 
+            " You have selected " + characterSelect.charAt(0).toUpperCase() + newClass.slice(1);
+        }
+
+        var form = document.getElementById("charactersubmit");
     });
 });
 
-var characterSelect = document.getElementById("characters");
 
-if (form !== true){
-               alert('Please select a character');
-           }
-           else if (form == true){
-               console.log('You have selected' + ("characters"));
-           }
-
-   var form = document.getElementById("charactersubmit");
-
-       form.addEventListener("click", function() {
-    
-   });
-/* var characterArry = {
-    "Alex": {
-        "name": "Alex",
-        "id": "alex",
-        "occupation": "fighter"
-    },
-    "Akuma": {
-        "name": "Akuma",
-        "id": "akuma",
-        "occupation": "fighter"
-    },
-    "Balrog": { 
-        "name": "Balrog",
-        "id": "balrog",
-        "occupation": "fighter"
-    },
-    "Bison": {
-        "name": "M. Bison",
-        "id": "bison",
-        "occupation": "fighter"
-    },
-    "Blanka": {
-        "name": "Blanka",
-        "id": "blanka",
-        "occupation": "fighter"
-    },
-    "Cammy": {
-        "name": "Cammy",
-        "id": "cammy",
-        "occupation": "fighter"
-    },
-    "Chun Li": {
-        "name": "Chun Li",
-        "id": "chun-li",
-        "occupation": "fighter"
-    },
-    "Deejay": {
-        "name": "Deejay",
-        "id": "deejay",
-        "occupation": "fighter"
-    },
-    "Dhalsim": {
-        "name": "Dhalsim",
-        "id": "dhalsim",
-        "occupation": "fighter"
-    },
-    "Dudley": {
-        "name": "Dudley",
-        "id": "dudley",
-        "occupation": "fighter"
-    },
-    "Fei-long": {
-        "name": "Fei-long",
-        "id": "fei-long",
-        "occupation": "fighter"
-    },
-    "Guile": {
-        "name": "Guile",
-        "id": "guile",
-        "occupation": "fighter"
-    },
-    "Guy": {
-        "name": "Guy",
-        "id": "guy",
-        "occupation": "fighter"
-    },
-    "Honda": {
-        "name": "Honda",
-        "id": "honda",
-        "occupation": "fighter"
-    },
-    "Ibuki": {
-        "name": "Ibuki",
-        "id": "Ibuki",
-        "occupation": "fighter"
-    },
-    "Ken": {
-        "name": "Ken",
-        "id": "ken",
-        "occupation": "fighter"
-    },
-    "Makoto": {
-        "name": "Makoto",
-        "id": "makoto",
-        "occupation": "fighter"
-    },
-    "Oro": {
-        "name": "Oro",
-        "id": "oro",
-        "occupation": "fighter"
-    },
-    "Ryu": {
-        "name": "Ryu",
-        "id": "ryu",
-        "occupation": "fighter"
-    },
-    "Sagat": {
-        "name": "Sagat",
-        "id": "sagat",
-        "occupation": "fighter"
-    },
-    "Skull": {
-        "name": "Skull",
-        "id": "skull",
-        "occupation": "fighter"
-    },
-    "T-Hawk": {
-        "name": "T-Hawk",
-        "id": "thawk",
-        "occupation": "fighter"
-    },
-    "Vega": {
-        "name": "Vega",
-        "id": "vega",
-        "occupation": "fighter"
-    },
-    "Yang": {
-        "name": "Yang",
-        "id": "yang",
-        "occupation": "fighter"
-    },
-    "Yun": {
-        "name": "Yun",
-        "id": "yun",
-        "occupation": "fighter"
-    },
-    "Zangief": {
-        "name": "Zangief",
-        "id": "zangief",
-        "occupation": "fighter"
-    }
-}; */
