fda46ae98c506b9d346a11b01f97ecf76a31808e
cdlay67 <dclaydowning@gmail.com>
2019-01-05 08:43:54 -0600

    Clean up version control

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
-window.onload = function () {
+window.onload = function(){
     alert('please choose a character');
-}
+};
 
-$(document).ready(function () {
-    $('img').click(function (e) {
-        var newClass = $(this).attr("id");
-        var oldClass = $("#characters").attr('class');
-        $("#characters").fadeOut(function () {
-            $("#characters").removeClass(oldClass).addClass(newClass).fadeIn('slow');
+$(document).ready(function(){
+    $('img').click(function(e){
+        var newClass = $(this).attr('id');
+        var oldClass = $('#characters').attr('class');
+
+        $('#characters').fadeOut(function(){
+            $('#characters').removeClass(oldClass).addClass(newClass).fadeIn('slow');
         });
-        $("#charactersname").text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
-        $(this).css("background-color", "#00cc99");
-        var characterSelect = $("#characters").attr('class');
-        document.getElementById("charselect").innerHTML =
-            " You have selected " + characterSelect.charAt(0).toUpperCase() + newClass.slice(1);
+        $('#charactersname').text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
+        $(this).css('background-color', '#00cc99');
+        var characterSelect = $('#characters').attr('class');
+
+        document.getElementById('charselect').innerHTML =
+            ' You have selected ' + characterSelect.charAt(0).toUpperCase() + newClass.slice(1);
     });
 });
 
-function myFunction() {
-    window.location = "menu.html"; 
+function myFunction(){
+    window.location = 'menu.html';
 }
--- js/vs.js
+++ js/vs.js
@@ -1,23 +1,25 @@
-window.onload = function () { // alerts the user to select a character
+window.onload = function(){ // alerts the user to select a character
     alert('Players please choose your character! Click the next button to start.');
-}
+};
 
-$(document).ready(function () {
-    $('img').click(function () {
-        var newClass = $(this).attr("id");
-        var oldClass = $("#characters").attr('class');
-        $("#characters").fadeOut(function () {
-            $("#characters").removeClass(oldClass).addClass(newClass).fadeIn('slow');
+$(document).ready(function(){
+    $('img').click(function(){
+        var newClass = $(this).attr('id');
+        var oldClass = $('#characters').attr('class');
+
+        $('#characters').fadeOut(function(){
+            $('#characters').removeClass(oldClass).addClass(newClass).fadeIn('slow');
         });
-        $("#charactersname").text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
-        $(this).css("background-color", "#00cc99");
-        var characterSelect = $("#characters").attr('class');
-        document.getElementById("charselect").innerHTML = " You have selected " + characterSelect.charAt(0).toUpperCase() + newClass.slice(1);
+        $('#charactersname').text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
+        $(this).css('background-color', '#00cc99');
+        var characterSelect = $('#characters').attr('class');
+
+        document.getElementById('charselect').innerHTML = ' You have selected ' + characterSelect.charAt(0).toUpperCase() + newClass.slice(1);
     });
 });
 
-function playerOne() {
-    confirm("Player 1 please select your character!");
+function playerOne(){
+    confirm('Player 1 please select your character!');
     toggle();
 }
 
