08afac3966bd1ca33f49776567b2217dc9a3f05a
cdlay67 <dclaydowning@gmail.com>
2019-01-05 10:40:16 -0600

    implement working background

---
+++ js/selectbg.js
@@ -0,0 +1,28 @@
+/* global $ */
+var playerOneChoice = localStorage.getItem('Player1');
+var playerTwoChoice = localStorage.getItem('Player2');
+
+// do what you want with those choices!
+
+function selectBackground(){
+    var $selectbg = $('#selectbg');
+    var $this = $(this);
+    var newClass = $this.attr('id');
+    var oldClass = $selectbg.attr('class');
+    var bgSelect = '';
+
+    $selectbg.fadeOut(() => $selectbg
+        .removeClass(oldClass)
+        .addClass(newClass)
+        .fadeIn('slow')
+    );
+
+    $('#selectbgname').text(newClass.charAt(0).toUpperCase() + newClass.slice(1));
+    $this.css('background-color', '#00cc99');
+
+    bgSelect = $('#selectbg').attr('src', 'spain.jpg');
+
+    document.getElementById('bgselect').innerHTML;
+}
+
+$('img').click(selectBackground);
