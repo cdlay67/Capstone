143029bac64ccd722d4b4e2e1ad409122d14488a
cdlay67 <dclaydowning@gmail.com>
2019-01-12 11:49:34 -0600

    updating repo

--- js/result.js
+++ js/result.js
@@ -7,10 +7,16 @@ var p2 = document.getElementById('player2');
 var background = document.getElementById('background');
 
 
-p1.innerHTML = ` <img id="Player1" alt="" src="../images/${playerOneChoice}/${playerOneChoice}.png" />`;
+p1.innerHTML = ` <img id="Player1"  style="width:100%; height:100%;" alt="" src="../images/${playerOneChoice}/${playerOneChoice}.png" />`;
 
-p2.innerHTML = ` <img id="Player2" alt="" src="../images/${playerTwoChoice}/${playerTwoChoice}.png" />`;
+p2.innerHTML = ` <img id="Player2" style="width:100%; height:100%;" alt="" src="../images/${playerTwoChoice}/${playerTwoChoice}.png" />`;
 
 background.innerHTML = ` <img id="background" alt="" src="../${selectBackground}" />`;
 
-                                    
+function goToReset(){
+    window.location = 'index.html';
+}
+
+document
+    .querySelector('#reset')
+    .addEventListener('click', goToReset);
